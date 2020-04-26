e51fbea694a378fadbb552395743d0b10d5c180d
Siddharth Bansal <sidbansal@berkeley.edu>
2020-04-25 18:17:39 -0700

    Sync up

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Firestore = require('@google-cloud/firestore');
 
 const db = new Firestore({
-    projectId: 'Factly',
-    keyFilename: '/Downloads/e37dfd7e6e3b.json',
+    projectId: 'seismic-kingdom-275320',
+    keyFilename: './e37dfd7e6e3b.json',
 });
