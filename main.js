577b80fe7dc30968ffd379d760f0cb7ba61c6215
Siddharth Bansal <sidbansal@berkeley.edu>
2020-04-25 18:44:05 -0700

    Naren made us waste time with the git push -- fixed

--- main.js
+++ main.js
@@ -18,7 +18,9 @@ const Firestore = require('@google-cloud/firestore');
 
 const db = new Firestore({
     projectId: 'seismic-kingdom-275320',
-    keyFilename: './e37dfd7e6e3b.json',
+    // keyFilename: './e37dfd7e6e3b.json',
+    private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC4YGs+pX14lDly\nKIUAdEpoBedPk9Mrj3jJSJeHjvKCL3cwtd5Tfzev63ZseXqfaoRKhSYToVfk43TU\ngzN91igQWGPUgmEa07mtCLIBNxuw36VpL2lIHKO92FcL+lnEjBz51pYjPTs/XAHU\naf3/lXOuld3DtllpXueXxoFaJt527J5NJdGdLcjGEMtuB6RzK2hIhpAiE5Ke3MZv\nbjU8TFTcSqdPzutbwmVbLZEehaGJp6DkI8AKGubFX24oUYDSmc6kRvNTUA5sTHqi\nsLZAPAw02RssYDNvlVY/rZfCJW0SsTX82RTNy4QQX1qsuDyRfo6LjkwR2/zOeUKg\nBbygzqRXAgMBAAECggEABZk36HER/EVT8Tl1wGvHjp0PRwVQFjSHEGWZ5E3c40MZ\ntWrwU0y7niXiNK0Doi+T0Md2PoZZquOVOpa5S4rjMIvejCdhV3UyYCSxcwP/XUGN\nPkJoSf5EopEdI4n4i9VNU/DGIk/LqsLac168jbQ2DQkT8Mtqr55oHd2zM8+WVboQ\nZabPYSZ0bJ3pX+0V3twFgSKM5158MxDd13fm8ZYDhSj4fVHJChqvqULq8E5O9x+U\n7MprQNqZ5xQ8r9P4W0Ws0XStVl8I9ayLX/GbCMrIv4OU7//JU8cro5VWOHwLTkwR\nvghGHCFGROdo1Icy4yO5/udKQAxsXDe7qtCu536SsQKBgQDlnQbKE2v9ORSiZpMP\nvdFSh5AkxuRPjiTMsrgHsy46UU4IuIx77MId7WFSUKAhSAjZcpEEh2vKWSuimaM0\nrApE/dqIk1z4yWO3xZtASVqnYoDJ+1CL2WaX4k1Ie0czz6A4cBGCTIgalHYQunZ3\nG2Y9nxdGgnzXXRkoeX7rxj5xmwKBgQDNkJO0cJBza8B4jS9loc/gFuOh6UkLznV6\nmDW9KtPc5oSDBh1XAW6vYR3HhfQAtyE3kpcZxVcFkLgSC6M13Trg+2SYq1GYPyVY\niqBFB17AYJT6ij8U/UpZJ3pSIUUqS6MfwYVubtFh//DIy+riO96Js/Qr+xaJ9ZvO\nse5H9Fvx9QKBgQCMTAIV/wAA3ZvKDzrUZ3s7HjHpBs4B/ixmFt1zdvxMDdnMYlcU\ncXm6p4zLqw/sxgMCOop5ZfVq0kljNNUQHf64YRPiV4h7SzJu24MdKLUHiZx/zdtN\nWCzjqRbnrsu20KppUbChGPogo43wcr5n0IoMJOUn5Bs7zFrO7E2s6pjE+QKBgQCH\nCnxN5Eiw2k9lonJJlMRH1SGTIvrOm8HHWKis7a6ihnj0zhaGKrW41tbGVTRXJ/bV\nWCKVVnN1f328tRJx9zSPyfZq6NjAaHMzOFZTIqVXQdt8v0bzlfW6ndmYjKywPWIb\nIXlzc69vbsCSWIukNWpbEfHM1rUWylehsHf1owyn5QKBgECuHyq3BHMJ6huQOFoZ\nuuOzAEbhjzeUsFlbc+SCrkxSkoK7pZeyNz/gRGmt5Lsu/qWqXZS+pLNP09Tc2j87\n4o6yYnNMW4Ank4Y9rvMuH++F/nUohRj9wAYjAO9G0KTbnWWMPKqNSI7v4qKhiKDD\nTa+O0eOmd8h9po3JEistwKN2\n-----END PRIVATE KEY-----\n",
+    client_email: "andy-zhao@seismic-kingdom-275320.iam.gserviceaccount.com"
 });
 
 let docRef = db.collection('domains').doc('https://time.com/5826108/unemployment-us-economy-coronavirus/');
